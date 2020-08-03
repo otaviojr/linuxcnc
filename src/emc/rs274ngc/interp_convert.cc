ebea72cb046c86fa3b6fb37b87b3a7c0042c952d
Otávio Ribeiro <otavio.ribeiro@gmail.com>
2020-08-03 20:58:17 -0300

    Update interp_convert.cc

--- src/emc/rs274ngc/interp_convert.cc
+++ src/emc/rs274ngc/interp_convert.cc
@@ -3224,6 +3224,8 @@ int Interp::convert_m(block_pointer block,       //!< pointer to a block of RS27
     }
   }
 
+  printf("MCODE");
+
  if (IS_USER_MCODE(block,settings,7) && ONCE_M(7)) {
     return convert_remapped_code(block, settings, STEP_M_7, 'm',
 				   block->m_modes[7]);
